4feff4884c6076f51e56b475cc7f6ac1cce5aa35
CarvajalSolache <carvajalsolache1@gmail.com>
2022-11-30 18:32:11 -0600

    mismo pero en cadena principal

---
+++ archviso desiciovo.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+
+void main(void)
+{
+	char nombre[500];
+	char cadenas[3300];
+	char buffer[3500][20];
+	int caracter = 0, conta = 0, caracter2 = 0, caracter3 = 0;
+
+	FILE* fp;
+	scanf_s("%s", nombre, 256);
+	fopen_s(&fp, nombre, "r");
+	if (fp != NULL)
+	{
+		while (!feof(fp))
+		{
+			fscanf_s(fp, "%s", cadenas, 300);
+			caracter = 0;
+			caracter3 = 0;
+			caracter2 = 0;
+			while (cadenas[caracter])
+			{
+				if (cadenas[caracter] == ')' || cadenas[caracter] == '(' || cadenas[caracter] == '.' || cadenas[caracter] == ',')
+					caracter++;
+				else
+				{
+					if (cadenas[caracter] != '(' || cadenas[caracter] != ')' || cadenas[caracter] != '.' || cadenas[caracter] != ',')
+					{
+						while (cadenas[caracter] != '(' && cadenas[caracter] != ')' && cadenas[caracter] != '.' && cadenas[caracter] != '\0' && cadenas[caracter] != ',')
+						{
+							caracter3 = 0;
+							buffer[conta][caracter2++] = cadenas[caracter++];
+							buffer[conta][caracter2] = '\0';
+						}
+						conta++;
+					}
+				}
+			}
+		}
+		fclose(fp);
+	}
+	else
+	{
+		printf("nose puedo");
+	}
+	caracter = 0;
+	buffer[conta][caracter] = '\0';
+	//estadistica comienza en 1
+	//int estadistica = 1;
+	//conta = 1; int conta2 = 0;
+	//while (buffer[conta2][caracter])
+	//{
+	//	if (conta2 == conta)
+	//		conta++;
+	//	else
+	//	{ 
+	//		while ((strcmp(buffer[conta2], buffer[conta]) == 0))
+	//		{
+	//			estadistica++, conta++;
+	//		}
+	//		printf("%i\n", estadistica);
+	//		conta2++, conta = 0;
+	//		estadistica = 1;
+	//	}
+	//}
+	buffer[conta][0] = '\0';
+	conta = 0, caracter2 = 0;
+	while (buffer[conta][caracter2])
+	{
+		while (buffer[conta][caracter2])
+			printf("%c", buffer[conta][caracter2++]);
+		conta++, caracter2 = 0;
+		printf("\n");
+	}
+	printf("\nhay %i cadenas", conta);
+}
+
